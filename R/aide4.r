dd668389d4aa2f2b1f03e3029d1236b69deb9df6
Alain LEFEBVRE <Alain.Lefebvre@ifremer.fr>
2013-11-15 12:00:45 +0000

    version 1.5

--- R/aide4.r
+++ R/aide4.r
@@ -2,7 +2,7 @@ Aide4 <- function () {
 
 aide4 <- tktoplevel()
 tktitle(aide4) <- "Help3 - TimeSeries_building"
-AIDE4 <- tktext(aide4, height=46, width=120)
+AIDE4 <- tktext(aide4, height=50, width=120)
 tkpack(AIDE4)
 tkinsert(AIDE4, "end", paste("This is the forth panel of the interface" ,"\n\n",
 
@@ -11,23 +11,25 @@ tkinsert(AIDE4, "end", paste("This is the forth panel of the interface" ,"\n\n",
 
 "      _The <Diagnostics> box, propose different options that give some extra informations about your time series.","\n\n",
 
-"       <Spectrum analysis> estimates the spectral density of the time series. Pick value","\n",
-"       in the figure show you the major frequencies of your time series. If your time step","\n",
+"       <Spectrum analysis> estimates the spectral density of the time series. Select the pick value in the figure", "\n",
+"       and the interface will display the corresponding cycle. If your time step","\n",
 "       is monthly and the major frequency is 1 then the time series have a cycle of 12 months." ,"\n",
 "       A time series could have more than one identified frequency.","\n\n",
 
 "       <Autocorrelation> compute an autocorrelation function on the time series and display","\n",
 "       a figure of the result.","\n\n",
 
-"       <Shapiro normality test>, test the normality of the time series distribution.","\n\n",
+"       <Shapiro normality test>, test the normality of the time series distribution and display a Q-Q Plot.","\n\n",
 
 "       <Anomaly (color.plot)> draw a color plot by year each time.step (months or weeks)","\n",
 "       minus the mean of the time.step of all years. Red colors show positive anomalies and","\n",
 "       blue colors negative anomalies. ","\n\n",
 
+"       <Anomaly (bar.plot)> display an anomaly barplot (years to weeks time step).","\n",
+"       Red colors show positive anomalies and blue colors negative anomalies.","\n\n",
+
 "       <Seasonal decomposition> decompose a time series into seasonal, trend and irregular components using","\n",
 "       loess and display the results in a figure (with remainder=residuals).","\n\n\n",
-
 
 "      _In the <Trend analysis> box you can perform a temporal trend analysis on the time series","\n",
 "       you compute through the interface.","\n\n",
